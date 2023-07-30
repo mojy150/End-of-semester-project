2433ab4f24c5cd5e462c8aec6465045d334f7758
mojy150 <mojy429@gmail.com>
2023-07-30 19:36:30 +0330

    add js in static

---
+++ saghi/static/js/app.js
@@ -0,0 +1,20 @@
+const user = "admin";
+const pass = "admin";
+let isLogin;
+
+// const login = () => {
+//   const username = document.getElementById("username");
+//   console.log(username);
+//   const password = document.getElementById("password");
+//   if (user == username.value && pass == password.value) {
+//     alert("ورود موفقیت آمیز");
+//     isLogin = true;
+//     console.log(isLogin);
+//     return isLogin;
+//   } else {
+//     alert("دوباره وارد شوید");
+//     isLogin = false;
+//   }
+// };
+
+console.log(isLogin);
