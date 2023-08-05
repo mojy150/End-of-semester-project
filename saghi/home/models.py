baedd3f728bcf96cb82d336d761d43b49ea1d273
mohammad reza azizi <78251072+mojy150@users.noreply.github.com>
2023-08-06 00:08:14 +0330

    create tables Country & Resolution

--- saghi/home/models.py
+++ saghi/home/models.py
@@ -16,14 +16,24 @@ class Director(models.Model):
     def __str__(self):
         return f"{self.director}"
 
+class Country(models.Model):
+    country = models.CharField(max_length=70,unique=True)
+    def __str__(self):
+        return f"{self.country}"
+
+class Resolution(models.Model):
+    resolution = models.CharField(max_length=50,unique=True)
+    def __str__(self):
+        return f"{self.resolution}"
+
 class Movie(models.Model):
     title = models.CharField(max_length=300)
-    genre = models.ManyToManyField(Genre,related_name="genre_rel_home")
-    country = models.CharField(max_length=250)
-    resolution = models.CharField(max_length=50)
-    actor = models.ManyToManyField(Actor, related_name="actor_rel_home")
-    director = models.ManyToManyField(Director,related_name="director_rel_home")
-    description = models.TextField()
+    genre = models.ManyToManyField(Genre,related_name="genre_rel_home",null=True, blank=True)
+    country = models.ManyToManyField(Country,related_name="country_rel_home",null=True, blank=True)
+    resolution = models.ManyToManyField(Resolution,related_name="resolution_rel_home",null=True, blank=True)
+    actor = models.ManyToManyField(Actor, related_name="actor_rel_home",null=True, blank=True)
+    director = models.ManyToManyField(Director,related_name="director_rel_home",null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
     IMDB_point = models.FloatField()
     poster = models.ImageField(null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
