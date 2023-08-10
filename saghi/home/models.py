da184d526586f3686aa2ac6b998e19d37e194063
mohammad reza azizi <78251072+mojy150@users.noreply.github.com>
2023-08-10 09:54:09 +0330

    edit minor

--- saghi/home/models.py
+++ saghi/home/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from datetime import datetime
 
 class Genre(models.Model):
-    genre = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50,unique=True)
     def __str__(self):
         return f"{self.genre}"
 
