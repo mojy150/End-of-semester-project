1c1f8b81df80701e19c13b61c896128ab4762932
mohammad reza azizi <78251072+mojy150@users.noreply.github.com>
2023-08-10 08:49:22 +0330

    connect movie page & index with urls

--- saghi/home/urls.py
+++ saghi/home/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.home),
-    path('movie/<int:movie_id>/',views.movie_page),
+    path('movie/<int:movie_id>/',views.movie_page,name="movie"),
 ]
