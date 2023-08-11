4ad1c111a0815793b8051feee79ee8fef2320671
mohammad reza azizi <78251072+mojy150@users.noreply.github.com>
2023-08-11 23:57:38 +0330

    create form for signup page

--- saghi/accounts/views.py
+++ saghi/accounts/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import UserRegistrationForm
+from django.contrib.auth.models import User
 
 def login(request):
     return render(request,'login.html')
 
 def signup(request):
-    return render(request,'signup.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd["username"],cd["email"],cd["password"])        
+    else:
+        form = UserRegistrationForm()
+    return render(request,'signup.html',{"form":form})
 
 def profile(request):
     return render(request,'panel.html')
