fedbd6a53ab1b7c830b05bc1b6a1cc0a1b27fcce
mohammad reza azizi <78251072+mojy150@users.noreply.github.com>
2023-08-11 21:44:43 +0330

    add name to index page

--- saghi/home/urls.py
+++ saghi/home/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home),
+    path('',views.home,name="home"),
     path('movie/<int:movie_id>/',views.movie_page,name="movie"),
 ]
